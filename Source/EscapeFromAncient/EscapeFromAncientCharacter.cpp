f80e56cbcc86bf7f87bfdf5e7c8c4898b5ee45be
Artem <hanter.veniaminov@gmail.com>
2024-04-01 21:00:16 +0300

    Update

--- Source/EscapeFromAncient/EscapeFromAncientCharacter.cpp
+++ Source/EscapeFromAncient/EscapeFromAncientCharacter.cpp
@@ -9,6 +9,8 @@
 #include "GameFramework/PlayerController.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Materials/Material.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "Engine/World.h"
 
 AEscapeFromAncientCharacter::AEscapeFromAncientCharacter()
@@ -47,5 +49,137 @@ AEscapeFromAncientCharacter::AEscapeFromAncientCharacter()
 
 void AEscapeFromAncientCharacter::Tick(float DeltaSeconds)
 {
+	MovementTickMy(DeltaSeconds);
+
     Super::Tick(DeltaSeconds);
+
+}
+
+void AEscapeFromAncientCharacter::SetupPlayerInputComponent(UInputComponent* NewInputComponent)
+{
+	Super::SetupPlayerInputComponent(NewInputComponent);
+
+	NewInputComponent->BindAxis(TEXT("MoveForward"), this, &AEscapeFromAncientCharacter::InputAxisX);
+	NewInputComponent->BindAxis(TEXT("MoveRight"), this, &AEscapeFromAncientCharacter::InputAxisY);
+
+}
+
+void AEscapeFromAncientCharacter::InputAxisX(float value)
+{
+	AxisX = value;
+}
+
+void AEscapeFromAncientCharacter::InputAxisY(float value)
+{
+	AxisY = value;
+}
+
+void AEscapeFromAncientCharacter::MovementTickMy(float deltaTime)
+{
+	AddMovementInput(FVector(1.0f, 0.0f, 0.0f), AxisX);
+	AddMovementInput(FVector(0.0f, 1.0f, 0.0f), AxisY);
+
+	APlayerController* myController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	if (myController)
+	{
+		FHitResult HitResult;
+
+		myController->GetHitResultUnderCursorByChannel(ETraceTypeQuery::TraceTypeQuery6, false, HitResult);
+
+		FRotator R = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), HitResult.Location);
+
+		SetActorRotation(FQuat(FRotator(0.0f, R.Yaw, 0.0f)));
+	}
+}
+
+void AEscapeFromAncientCharacter::CharacterUpdate()
+{
+	float ResSpeed = 800.0f;
+
+	switch (MovementState)
+	{
+		case EMovementState::Stand_State:
+			ResSpeed = CharacterSpeed.RunSpeed;
+		break;
+		case EMovementState::Aim_State:
+			ResSpeed = CharacterSpeed.AimSpeed;
+			break;
+		case EMovementState::Walk_State:
+			ResSpeed = CharacterSpeed.WalkSpeed;
+			break;
+		case EMovementState::WalkAim_State:
+			ResSpeed = CharacterSpeed.WalkAimSpeed;
+			break;
+		case EMovementState::Run_State:
+			ResSpeed = CharacterSpeed.RunSpeed;
+			break;
+		case EMovementState::RunAim_State:
+			ResSpeed = CharacterSpeed.RunAimSpeed;
+			break;
+		case EMovementState::Sprint_State:
+			ResSpeed = CharacterSpeed.SprintSpeed;
+			break;
+		default:
+			break;
+	}
+
+	GetCharacterMovement()->MaxWalkSpeed = ResSpeed;
+}
+
+void AEscapeFromAncientCharacter::ChangeMovementState()
+{
+	if (Aim)
+	{
+		if (Speed == 0)
+		{
+			MovementState = EMovementState::Aim_State;
+		}
+		else
+		{
+			MovementState = EMovementState::RunAim_State;
+
+			if (Walk)
+			{
+				MovementState = EMovementState::WalkAim_State;
+			}
+			else if (Run)
+			{
+				MovementState = EMovementState::RunAim_State;
+			}
+			else if (Sprint)
+			{
+				MovementState = EMovementState::Sprint_State;
+			}
+		}
+	}
+	else
+	{
+		if (Speed == 0)
+		{
+			MovementState = EMovementState::Stand_State;
+		}
+		else
+		{
+			MovementState = EMovementState::Run_State;
+
+			if (Walk)
+			{
+				MovementState = EMovementState::Walk_State;
+			}
+			else if (Run)
+			{
+				MovementState = EMovementState::Run_State;
+			}
+			else if (Sprint)
+			{
+				MovementState = EMovementState::Sprint_State;
+			}
+			else
+			{
+				MovementState = EMovementState::Run_State;
+			}
+		}
+	}
+
+	CharacterUpdate();
 }
--- Source/EscapeFromAncient/EscapeFromAncientCharacter.h
+++ Source/EscapeFromAncient/EscapeFromAncientCharacter.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+
+#include "NewTypes.h"
+
 #include "EscapeFromAncientCharacter.generated.h"
+
 
 UCLASS(Blueprintable)
 class AEscapeFromAncientCharacter : public ACharacter
@@ -16,6 +20,8 @@ public:
 
 	// Called every frame.
 	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* NewInputComponent);
 
 	/** Returns TopDownCameraComponent subobject **/
 	FORCEINLINE class UCameraComponent* GetTopDownCameraComponent() const { return TopDownCameraComponent; }
@@ -30,5 +36,48 @@ private:
 	/** Camera boom positioning the camera above the character */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class USpringArmComponent* CameraBoom;
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		EMovementState MovementState = EMovementState::Stand_State;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FCharacterSpeed CharacterSpeed;
+
+	UFUNCTION()
+		void InputAxisX(float value);
+	UFUNCTION()
+		void InputAxisY(float value);
+
+		float AxisX = 0.0f;
+		float AxisY = 0.0f;
+
+	UFUNCTION()
+		void MovementTickMy(float deltaTime);
+
+	UFUNCTION()
+		void CharacterUpdate();
+	UFUNCTION(BlueprintCallable)
+		void ChangeMovementState();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool Stand = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool Aim = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool Walk = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool Run = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool Sprint = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float Speed = 0.0f;
 };
 
+/*
+Aim
+Walk
+Run
+Sprint
+WalkAim
+RunAim
+*/
